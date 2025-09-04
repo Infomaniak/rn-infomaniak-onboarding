e9fe078e3d3654276ca05bc2b0efee78bdb21d91
Philippe Weidmann <philippe.weidmann@infomaniak.com>
2025-09-04 08:17:49 +0200

    feat: Add onboarding library poc

--- ios/RNInfomaniakOnboardingView.swift
+++ ios/RNInfomaniakOnboardingView.swift
@@ -1,38 +1,35 @@
 import ExpoModulesCore
-import WebKit
+import InfomaniakOnboarding
+import SwiftUI
 
-// This view will be used as a native component. Make sure to inherit from `ExpoView`
-// to apply the proper styling (e.g. border radius and shadows).
 class RNInfomaniakOnboardingView: ExpoView {
-  let webView = WKWebView()
-  let onLoad = EventDispatcher()
-  var delegate: WebViewDelegate?
+    let onboardingViewController = OnboardingViewController(configuration: .init(headerImage: nil,
+                                                                                 slides: [
+                                                                                     .init(
+                                                                                         backgroundImage: UIImage(
+                                                                                             systemName: "circle"
+                                                                                         )!,
+                                                                                         backgroundImageTintColor: nil,
+                                                                                         content: .illustration(
+                                                                                             UIImage(systemName: "circle")!
+                                                                                         ),
+                                                                                         bottomView: Text("Hello")
+                                                                                     )
+                                                                                 ],
+                                                                                 pageIndicatorColor: nil,
+                                                                                 isScrollEnabled: true,
+                                                                                 dismissHandler: nil,
+                                                                                 isPageIndicatorHidden: false))
+    let onLoad = EventDispatcher()
 
-  required init(appContext: AppContext? = nil) {
-    super.init(appContext: appContext)
-    clipsToBounds = true
-    delegate = WebViewDelegate { url in
-      self.onLoad(["url": url])
+    required init(appContext: AppContext? = nil) {
+        super.init(appContext: appContext)
+        clipsToBounds = true
+
+        addSubview(onboardingViewController.view)
     }
-    webView.navigationDelegate = delegate
-    addSubview(webView)
-  }
 
-  override func layoutSubviews() {
-    webView.frame = bounds
-  }
-}
-
-class WebViewDelegate: NSObject, WKNavigationDelegate {
-  let onUrlChange: (String) -> Void
-
-  init(onUrlChange: @escaping (String) -> Void) {
-    self.onUrlChange = onUrlChange
-  }
-
-  func webView(_ webView: WKWebView, didFinish navigation: WKNavigation) {
-    if let url = webView.url {
-      onUrlChange(url.absoluteString)
+    override func layoutSubviews() {
+        onboardingViewController.view.frame = bounds
     }
-  }
 }
