b541ba1445d342e28d8a250c328f594003593447
Philippe Weidmann <philippe.weidmann@infomaniak.com>
2025-09-04 08:17:20 +0200

    feat: Add initial login config and onboarding config

--- src/RNInfomaniakOnboarding.types.ts
+++ src/RNInfomaniakOnboarding.types.ts
@@ -1,19 +1,40 @@
 import type { StyleProp, ViewStyle } from 'react-native';
 
-export type OnLoadEventPayload = {
-  url: string;
+export type Slide = {
+  backgroundImageName?: string;
+  backgroundImageTintColor?: string; // Color mapped from string
+  illustrationName?: string;
+  animationName?: string;
+  title?: string; // defaults to ""
+  subtitle?: string; // defaults to ""
 };
 
-export type RNInfomaniakOnboardingModuleEvents = {
-  onChange: (params: ChangeEventPayload) => void;
+export type OnboardingConfiguration = {
+  headerImageName?: string;
+  slides?: Slide[];
+  pageIndicatorColor?: string; // Color mapped from string
+  isScrollEnabled?: boolean; // defaults to true
+  isPageIndicatorHidden?: boolean; // defaults to false
 };
 
-export type ChangeEventPayload = {
-  value: string;
+export type LoginConfiguration = {
+  clientId: string;
+  loginURL?: string; // defaults to "https://login.infomaniak.com/"
+  redirectURI?: string; // defaults to "appbundleid://oauth2redirect"
+};
+
+export type OnLoginSuccessEventPayload = {
+  accessToken: string;
+};
+
+export type OnLoginErrorEventPayload = {
+  error: string;
 };
 
 export type RNInfomaniakOnboardingViewProps = {
-  url: string;
-  onLoad: (event: { nativeEvent: OnLoadEventPayload }) => void;
+  loginConfiguration: LoginConfiguration;
+  onboardingConfiguration: OnboardingConfiguration;
+  onLoginSuccess?: (event: { nativeEvent: OnLoginSuccessEventPayload }) => void;
+  onLoginError?: (event: { nativeEvent: OnLoginErrorEventPayload }) => void;
   style?: StyleProp<ViewStyle>;
 };
