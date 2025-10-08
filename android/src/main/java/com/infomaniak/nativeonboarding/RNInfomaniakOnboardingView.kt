7a4b3fe646904d0dffab0529916ad2f8c4eceae5
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2025-10-08 10:39:45 +0200

    feat: Add compose to the project

--- android/src/main/java/com/infomaniak/nativeonboarding/RNInfomaniakOnboardingView.kt
+++ android/src/main/java/com/infomaniak/nativeonboarding/RNInfomaniakOnboardingView.kt
@@ -1,31 +1,58 @@
 package com.infomaniak.nativeonboarding
 
 import android.content.Context
-import android.webkit.WebView
-import android.webkit.WebViewClient
-import android.widget.TextView
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import expo.modules.kotlin.AppContext
-import expo.modules.kotlin.viewevent.EventDispatcher
 import expo.modules.kotlin.views.ExpoView
 
 class RNInfomaniakOnboardingView(context: Context, appContext: AppContext) : ExpoView(context, appContext) {
-  // Creates and initializes an event dispatcher for the `onLoad` event.
-  // The name of the event is inferred from the value and needs to match the event name defined in the module.
-  private val onLoad by EventDispatcher()
+    // Creates and initializes an event dispatcher for the `onLoad` event.
+    // The name of the event is inferred from the value and needs to match the event name defined in the module.
+    // private val onLoad by EventDispatcher()
 
-  // Defines a WebView that will be used as the root subview.
-  internal val webView = WebView(context).apply {
-    layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
-    webViewClient = object : WebViewClient() {
-      override fun onPageFinished(view: WebView, url: String) {
-        // Sends an event to JavaScript. Triggers a callback defined on the view component in JavaScript.
-        onLoad(mapOf("url" to url))
-      }
+    // Defines a WebView that will be used as the root subview.
+    // internal val webView = WebView(context).apply {
+    //     layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
+    //     webViewClient = object : WebViewClient() {
+    //         override fun onPageFinished(view: WebView, url: String) {
+    //             // Sends an event to JavaScript. Triggers a callback defined on the view component in JavaScript.
+    //             onLoad(mapOf("url" to url))
+    //         }
+    //     }
+    // }
+
+    init {
+        addView(ComposeView(context).apply {
+            setContent {
+                OnboardingViewContent()
+            }
+        })
     }
-  }
+}
 
-  init {
-    // Adds the WebView to the view hierarchy.
-    addView(TextView(context).apply { setText("Hello World!") })
-  }
+@Composable
+private fun OnboardingViewContent() {
+    Scaffold { contentPadding ->
+        Box(Modifier.padding(contentPadding)) {
+            Text("Hello world!", style = TextStyle(fontSize = 40.sp))
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun Preview() {
+    MaterialTheme {
+        OnboardingViewContent()
+    }
 }
