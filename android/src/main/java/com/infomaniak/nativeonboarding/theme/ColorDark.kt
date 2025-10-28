d977d9173c265d218f683e90b55dc63e7a671637
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2025-10-28 09:21:13 +0100

    feat: Setup material theme

--- android/src/main/java/com/infomaniak/nativeonboarding/theme/ColorDark.kt
+++ android/src/main/java/com/infomaniak/nativeonboarding/theme/ColorDark.kt
@@ -5,7 +5,7 @@ import androidx.compose.ui.graphics.Color
 private val rabbit = Color(0xFFF1F1F1)
 private val shark = Color(0xFF9F9F9F)
 
-val CustomDarkColorScheme = CustomColors(
+val CustomDarkColors = CustomColors(
     primaryTextColor = rabbit,
     secondaryTextColor = shark,
 )
