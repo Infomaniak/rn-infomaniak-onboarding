a0b45a0cb72c975ae412a7dd13aa5bc320fa15eb
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2025-10-30 12:48:14 +0100

    feat: Use provided argument colors to generate a material theme

---
+++ android/src/main/java/com/infomaniak/nativeonboarding/models/OnboardingArgumentColors.kt
@@ -0,0 +1,10 @@
+package com.infomaniak.nativeonboarding.models
+
+import androidx.compose.ui.graphics.Color
+
+data class OnboardingArgumentColors(
+    val primaryColorLight: Color,
+    val primaryColorDark: Color,
+    val onPrimaryColorLight: Color,
+    val onPrimaryColorDark: Color,
+)
