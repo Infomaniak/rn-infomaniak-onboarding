63daaede7c5b7a44b29312aa4821e37b017aefe7
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2025-12-01 17:01:10 +0100

    feat: Use shared login logic to connect and get a user

--- android/src/main/java/com/infomaniak/nativeonboarding/RNInfomaniakOnboardingView.kt
+++ android/src/main/java/com/infomaniak/nativeonboarding/RNInfomaniakOnboardingView.kt
@@ -1,17 +1,26 @@
 package com.infomaniak.nativeonboarding
 
 import android.content.Context
+import android.util.Log
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.core.graphics.toColorInt
+import com.infomaniak.core.auth.UserExistenceChecker
+import com.infomaniak.core.auth.utils.LoginUtils
+import com.infomaniak.core.network.NetworkConfiguration
+import com.infomaniak.lib.login.InfomaniakLogin
 import com.infomaniak.nativeonboarding.models.LoginConfiguration
 import com.infomaniak.nativeonboarding.models.OnboardingArgumentColors
 import com.infomaniak.nativeonboarding.models.OnboardingConfiguration
@@ -21,6 +30,7 @@ import com.infomaniak.nativeonboarding.theme.OnboardingTheme
 import expo.modules.kotlin.AppContext
 import expo.modules.kotlin.viewevent.EventDispatcher
 import expo.modules.kotlin.views.ExpoView
+import kotlinx.coroutines.launch
 
 class RNInfomaniakOnboardingView(context: Context, appContext: AppContext) : ExpoView(context, appContext) {
     // Creates and initializes an event dispatcher for the `onLoginSuccess` and `onLoginError` events.
@@ -28,13 +38,37 @@ class RNInfomaniakOnboardingView(context: Context, appContext: AppContext) : Exp
     private val onLoginSuccess by EventDispatcher()
     private val onLoginError by EventDispatcher()
 
+    private var infomaniakLogin: InfomaniakLogin? by mutableStateOf(null)
     private val pages = mutableStateListOf<Page>()
     private var onboardingArgumentColors by mutableStateOf<OnboardingArgumentColors?>(null)
+
+    private val userExistenceChecker = UserExistenceChecker { false }
 
     init {
         addView(ComposeView(context).apply {
             setContent {
-                OnboardingViewContent(pages, { onboardingArgumentColors })
+                infomaniakLogin?.let { infomaniakLogin ->
+                    val scope = rememberCoroutineScope()
+                    val context = LocalContext.current
+                    val launcher = rememberLauncherForActivityResult(StartActivityForResult()) { result ->
+                        scope.launch {
+                            val userLoginResult = LoginUtils.getLoginResultAfterWebView(
+                                result = result,
+                                context = context,
+                                infomaniakLogin = infomaniakLogin,
+                                userExistenceChecker = userExistenceChecker,
+                            )
+
+                            Log.e("gibran", "on login - userLoginResult: ${userLoginResult}")
+                        }
+                    }
+
+                    OnboardingViewContent(
+                        pages = pages,
+                        colors = { onboardingArgumentColors },
+                        onLoginRequest = { infomaniakLogin.startWebViewLogin(launcher) },
+                    )
+                }
             }
         })
     }
@@ -52,18 +86,36 @@ class RNInfomaniakOnboardingView(context: Context, appContext: AppContext) : Exp
     }
 
     fun setLoginConfig(config: LoginConfiguration?) {
-        // TODO
+        config?.let {
+            NetworkConfiguration.init(
+                appId = it.redirectURIScheme,
+                appVersionCode = it.appVersionCode,
+                appVersionName = it.appVersionName,
+            )
+
+            infomaniakLogin = InfomaniakLogin(
+                context = context,
+                loginUrl = it.loginUrl,
+                clientID = it.clientId,
+                appUID = it.redirectURIScheme,
+                accessType = null,
+            )
+        }
     }
 }
 
 private fun String.toColor(): Color = Color(toColorInt())
 
 @Composable
-private fun OnboardingViewContent(pages: SnapshotStateList<Page>, colors: () -> OnboardingArgumentColors?) {
+private fun OnboardingViewContent(
+    pages: SnapshotStateList<Page>,
+    colors: () -> OnboardingArgumentColors?,
+    onLoginRequest: () -> Unit
+) {
     OnboardingTheme(colors) {
         OnboardingScreen(
             pages = pages,
-            onLoginRequest = {},
+            onLoginRequest = onLoginRequest,
             onCreateAccount = {},
         )
     }
@@ -72,5 +124,5 @@ private fun OnboardingViewContent(pages: SnapshotStateList<Page>, colors: () -> 
 @Preview
 @Composable
 private fun Preview(@PreviewParameter(PagesPreviewParameter::class) pages: SnapshotStateList<Page>) {
-    OnboardingViewContent(pages, { null })
+    OnboardingViewContent(pages, { null }, {})
 }
